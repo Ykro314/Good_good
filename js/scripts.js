d7d69fe787c68b896c78c71a0b0b35b459345017
Ykro314 <lolkokpol@gmal.com>
2016-01-10 20:16:38 +0200

    дописал функционал сетки галереи, добавил функцию обрезки строки в статьях

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 (function() {
   
+/*=============================  Testimonals slider  ================================*/
+  
 var element = document.querySelectorAll( ".element" );
 var phrase = document.querySelectorAll(".testimonals__phrase");
 
@@ -30,6 +32,8 @@ for( var i = 0; i < element.length; i++ ) {
 };
   
   
+/*========================  Mobile navigation slide over  ===========================*/
+  
 var btn = document.querySelector( ".page-header__open-btn" );
 var menu = document.querySelector( ".page-header__main-navigation" );
 var icon = btn.firstElementChild;
@@ -54,6 +58,9 @@ btn.addEventListener( "click", function( event ) {
 }, false ); 
   
   
+  
+/*========================== Category button-list ===============================*/
+  
 var category_btn = document.querySelector( ".gallery-nav__category" );
 var category_list = document.querySelector( ".category-list" );
   
@@ -70,7 +77,74 @@ category_list.addEventListener( "click", function( event ) {
 })
 
 
+
+/*========================= Portfolio gallery grid buttons ==========================*/
+var gallery_panel = document.querySelector( ".gallery-nav" )
+var btn_square = document.querySelector( ".gallery-nav__grid-btn--square" );
+var btn_lines = document.querySelector( ".gallery-nav__grid-btn--lines" );
+var gallery = document.querySelector( ".gallery-container" );
+
+gallery_panel.addEventListener( "click", function( event ) {
+  
+  if( event.target == btn_square && btn_lines.classList.contains( "lines-active" ) ) {
+    event.target.classList.add( "square-active" );
+    btn_lines.classList.remove( "lines-active" );
+    gallery.classList.add( "gallery-grid--square" );
+    gallery.classList.remove( "gallery-grid--lines" );
+  }
+  
+  else if ( event.target == btn_lines && btn_square.classList.contains( "square-active" ) ) {
+    event.target.classList.add( "lines-active" );
+    btn_square.classList.remove( "square-active" );
+    gallery.classList.add( "gallery-grid--lines" );
+    gallery.classList.remove( "gallery-grid--square" );
+  }
+  
+})
+
+/*============================  Textcontent slice onload  ==========================*/
+
+var gallery_content = document.querySelectorAll( ".gallery-item__intro" );
+  
+document.addEventListener( "DOMContentLoaded", function( event ) {
+  
+  for( i = 0; i < gallery_content.length; i++ ) {
+    
+    if( gallery_content[i].textContent.length > 120 ) {
+      gallery_content[i].textContent =  gallery_content[i].textContent.substr(0, 120) + "...";
+    }
+    
+  }
+  
+})
+
 })();
+
+
+
+
+/*
+
+function addClickListener(btn, text) {
+  btn.addEventListener("click", function ( event ) {
+    console.log(text);
+  })
+}
+
+addClickListener( document.querySelector( ".page-header__search" ), "123123123" );
+addClickListener( document.querySelector( ".page-banner__title" ), "asdasdasda" );
+
+*/
+
+
+
+
+
+
+
+
+
+
 
 
 
